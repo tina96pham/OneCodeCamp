366668cba604fdcc0a8a6b82fc18393c628f70e5
tina96pham <tina96pham@gmail.com>
2024-03-26 19:06:57 -0400

    Add day 5 note

---
+++ Intro_to_JavaScript/index.js
@@ -0,0 +1,8 @@
+let firstName="John";
+console.log('Hello!',firstName);
+
+// 1. Should be meaningful and descriptive.
+// 2.SHould not start with a number.
+// 3. Should not contain space or hyphen.
+// 4. It is case sensitive.
+// 5. Cannot use reversed keywords.
